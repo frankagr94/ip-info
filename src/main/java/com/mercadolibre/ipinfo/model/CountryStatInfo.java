8e6a9bc67f6f71cfe0071253b1bc28cea514527b
Frank Gonzalez <Frank Gonzalez>
2021-02-23 22:14:31 -0500

    fix table name and added readme

--- src/main/java/com/mercadolibre/ipinfo/model/CountryStatInfo.java
+++ src/main/java/com/mercadolibre/ipinfo/model/CountryStatInfo.java
@@ -8,18 +8,14 @@ import lombok.Setter;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "coutry_stat_info",
+@Table(name = "country_stat_info",
         uniqueConstraints = {@UniqueConstraint(columnNames={"iso_code", "name"})},
         indexes = {
                 @Index(name = "fn_country_stat_info_index_iso_code", columnList = "iso_code"),
                 @Index(name = "fn_country_stat_info_index_name", columnList = "name"),
-                @Index(name = "fn_country_stat_info_index_distance", columnList = "distance"),
                 @Index(name = "fn_country_stat_info_index_unique_iso_code", columnList = "iso_code", unique = true),
                 @Index(name = "fn_country_stat_info_index_unique_name", columnList = "name", unique = true),
-                @Index(name = "fn_country_stat_info_index_unique_distance", columnList = "distance", unique = true),
-                @Index(name = "fn_country_stat_info_index_multi", columnList = "iso_code, name"),
-                @Index(name = "fn_country_stat_info_index_multi2", columnList = "iso_code, name, distance"),
-                @Index(name = "fn_country_stat_info_index_multi3", columnList = "iso_code, name, distance, invocations")
+                @Index(name = "fn_country_stat_info_index_multi", columnList = "iso_code, name")
         })
 @Setter
 @Getter
